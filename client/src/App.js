374a540ba5e30c08b0a3e1eb998eb33161d3be97
Ankit Deb <deb.ankit.work@gmail.com>
2024-03-28 01:01:06 +0530

    video-faceapi integration done

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,58 @@
 
 import './App.css';
+import * as faceapi from 'face-api.js';
+import React, { useState, useRef, useEffect } from 'react';
 import LandingPage from './components/pages/landingPage/LandingPage';
-// import Login from './components/credentials/login/Login';
+
 
 function App() {
+
+  // const [initializing, setInitializing] = useState(false);
+  // const videoRef = useRef();
+  // const canvasRef = useRef();
+  // const videoHeight = 480;
+  // const videoWidth = 640;
+
+  // useEffect(() => {
+  //   const loadModels = async () => {
+  //     const MODEL_URL = process.env.PUBLIC_URL + '/models';
+  //     setInitializing(true);
+  //     Promise.all([
+  //       faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+  //       faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+  //       faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+  //       faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+
+
+  //     ]).then(startVideo);
+  //   }
+  //   loadModels();
+  // }, [])
+
+
+  // const startVideo = () => {
+  //   navigator.getUserMedia(
+  //     { video: true },
+  //     stream => videoRef.current.srcObject = stream,
+  //     error => console.error('getUserMedia error:', error)
+  //   );
+  // };
+
+
+
+
   return (
     <>
-    <LandingPage />
+      {/* <span>{initializing ? 'Initializing' : 'Ready'}</span>
+      <video ref={videoRef} autoPlay muted height={videoHeight} width={videoWidth} />
+      <canvas ref={canvasRef} /> */}
+
+      <LandingPage />
     </>
   );
 }
 
 export default App;
+
+
+
--- client/src/components/pages/landingPage/LandingPage.jsx
+++ client/src/components/pages/landingPage/LandingPage.jsx
@@ -1,12 +1,83 @@
-import React from 'react';
 import './landingPage.css';
+import * as faceapi from 'face-api.js';
+import React, { useState, useRef, useEffect } from 'react';
 
 const LandingPage = () => {
+
+    const [initializing, setInitializing] = useState(false);
+    const videoRef = useRef();
+    const canvasRef = useRef();
+    const videoHeight = 403;
+    const videoWidth = 614;
+
+
+
+    useEffect(() => {
+        const loadModels = async () => {
+            const MODEL_URL = process.env.PUBLIC_URL + '/models';
+            setInitializing(true);
+            Promise.all([
+                faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+                faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+                faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+                faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+
+
+            ]).then(startVideo);
+        }
+        loadModels();
+    }, [])
+
+
+    const startVideo = () => {
+        navigator.getUserMedia(
+            { video: true },
+            stream => videoRef.current.srcObject = stream,
+            error => console.error('getUserMedia error:', error)
+        );
+    };
+
+
+
+
+
+    const handleVideoOnePlay = () => {
+        setInterval(async () => {
+            if (initializing) {
+                setInitializing(false);
+            }
+
+            console.log('Before detection');
+            console.log('Video element:', videoRef.current);
+
+            if (!videoRef.current) {
+                console.error('Video element not found');
+                return;
+            }
+
+            try {
+                const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+
+                console.log('Detections:', detections);
+            } catch (error) {
+                console.error('Error detecting faces:', error);
+            }
+        }, 100);
+    }
+
+
+
+
+
+
+
     return (
         <div className="container">
 
             <div className="header">
-                Welcome to Face Recognition Web App, with AI!
+
+                {/* un-comment it when testing  */}
+                <span>{initializing ? 'Initializing...' : 'Ready'}</span>
             </div>
 
             <div className="top">
@@ -35,8 +106,22 @@ const LandingPage = () => {
             </div>
 
             <div className="bottom">
+
+                <div className="image-container">
+                    Hello, I am the image here. Thanks!
+                </div>
                 <div className="video-container">
-                    Hola!
+
+                    <video ref={videoRef} autoPlay muted height={videoHeight} width={videoWidth} onPlay={handleVideoOnePlay} />
+                    <canvas ref={canvasRef} />
+                </div>
+            </div>
+
+
+            <div className="footer">
+                <div className="capture-details">
+                    I have all the capture details here. Yay!
+
                 </div>
             </div>
         </div>
